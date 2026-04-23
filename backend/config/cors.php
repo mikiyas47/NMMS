e106706026b3c988371ab95924ad24927e87e48e
Mikiyas Shemels <mikishemels@gmail.com>
2026-04-23 14:09:34 +0300

    Update CORS to allow Vercel frontend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5173', 'https://nmms-ochre.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
