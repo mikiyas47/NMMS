57074c98a3485096a41fa2e305737e1954bbb570
Mikiyas Shemels <mikishemels@gmail.com>
2026-05-01 15:08:44 +0300

    Add /fix-points route to easily run recalculation on Render

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fix-points', function () {
+    \Illuminate\Support\Facades\Artisan::call('mlm:fix-points');
+    return 'Points recalculated successfully! ' . \Illuminate\Support\Facades\Artisan::output();
+});
